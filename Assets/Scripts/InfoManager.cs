7e4b898066f165856754f6feca257fb2aa63c8cc
Liberess <58577796+Liberess@users.noreply.github.com>
2020-08-19 15:58:35 +0900

    Add InfoManager

---
+++ Assets/Scripts/InfoManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoManager : MonoBehaviour
+{
+    public Image panel;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            panel.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            panel.gameObject.SetActive(false);
+        }
+    }
+}
